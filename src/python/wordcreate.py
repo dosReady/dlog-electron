7d7c411a4a2178f069f53aaad5dc8c52fa2239b9
dos <dos>
2018-11-04 21:25:06 +0900

    옵션기능 완료, 메일머지 기능 구현완료 이제 다음단계 정리하는것이다.

--- src/python/wordcreate.py
+++ src/python/wordcreate.py
@@ -10,13 +10,15 @@ from mailmerge import MailMerge
 __mode__ = sys.argv[1]
 __prod_dir__ = os.path.dirname(os.path.realpath(__file__)) 
 __dev_dir__ = os.path.abspath(os.path.join(__prod_dir__, '..'))
-__this_dir__ = __prod_dir__ if __mode__ == 'porduction' else __dev_dir__
+__this_dir__ = __prod_dir__ if __mode__ == 'production' else __dev_dir__
 __conf_file__ = f'{__this_dir__}\\conf\\options.json'
     
 def mailmerge():
     f = open(__conf_file__,'r')
     data = json.loads(f.read())
-    print(data)
+    data['argv'] = []
+    
+    print(json.dumps(data))
     document = MailMerge(data['tempdir']+'\\name.docx')
     document.merge(제목='낼출근..')
     document.write(data['exportdir'] + '\\output123.docx')
--- src/renderer/main.js
+++ src/renderer/main.js
@@ -39,7 +39,8 @@ Vue.pyrouterFn = Vue.prototype.$pyrouterFn = async function (pyfile, args) {
     obj.argv.splice(0, 1)
     return obj
   } else {
-    const {stdout, stderr} = await execFile('python', [`${pathval}\\${pyfile}.py`, 'development', args])
+    // const {stdout, stderr} = await execFile('python', [`${pathval}\\${pyfile}.py`, 'development', args])
+    const {stdout, stderr} = await execFile(`${pathval}\\${pyfile}.exe`, ['development', args])
     if (stderr) throw stderr
     let obj = JSON.parse(stdout)
     obj.argv.splice(0, 1)
